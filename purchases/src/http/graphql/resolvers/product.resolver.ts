86dfe36a195a70f4eae8af4c072ef7ea418aae3d
RennanD <rennan.douglas@objetivopi.com.br>
2022-04-05 09:52:00 -0300

    feat(purchases): create a new product

--- purchases/src/http/graphql/resolvers/product.resolver.ts
+++ purchases/src/http/graphql/resolvers/product.resolver.ts
@@ -1,18 +1,24 @@
-// import { UseGuards } from '@nestjs/common';
-import { Resolver, Query } from '@nestjs/graphql';
+import { UseGuards } from '@nestjs/common';
+import { Resolver, Query, Mutation, Args } from '@nestjs/graphql';
 
-import { Product } from '../models/Product';
+import { Product } from '../models/product';
 
-// import { AuthorizationGuard } from '../../../http/auth/authorization.guard';
+import { AuthorizationGuard } from '../../../http/auth/authorization.guard';
 import { ProductsService } from '../../../services/products.service';
+import { CreateProductInput } from '../inputs/create-product-input';
 
 @Resolver('test')
 export class ProductResolver {
   constructor(private productsService: ProductsService) {}
 
   @Query(() => [Product])
-  // @UseGuards(AuthorizationGuard)
   async products() {
     return this.productsService.listAllProducts();
+  }
+
+  @Mutation(() => Product)
+  @UseGuards(AuthorizationGuard)
+  createProduct(@Args('data') data: CreateProductInput) {
+    return this.productsService.createProduct(data);
   }
 }
--- purchases/src/services/products.service.ts
+++ purchases/src/services/products.service.ts
@@ -1,5 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import slugify from 'slugify';
+
 import { PrismaService } from '../database/prisma/prisma.service';
+
+interface CreateProductData {
+  title: string;
+}
 
 @Injectable()
 export class ProductsService {
@@ -7,5 +13,24 @@ export class ProductsService {
 
   async listAllProducts() {
     return this.prisma.product.findMany();
+  }
+
+  async createProduct({ title }: CreateProductData) {
+    const slug = slugify(title, { lower: true });
+
+    const productWithSameSlug = await this.prisma.product.findUnique({
+      where: { slug },
+    });
+
+    if (productWithSameSlug) {
+      throw new Error('Another product with same name already exists');
+    }
+
+    return this.prisma.product.create({
+      data: {
+        title,
+        slug,
+      },
+    });
   }
 }
