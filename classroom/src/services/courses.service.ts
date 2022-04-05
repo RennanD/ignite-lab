f10b3514607a2c17c4dfe01e90b075c6f3b77d52
RennanD <rennan.douglas@objetivopi.com.br>
2022-04-05 15:09:14 -0300

    feat(classroom): create a new course

--- classroom/src/services/courses.service.ts
+++ classroom/src/services/courses.service.ts
@@ -1,5 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import slugify from 'slugify';
+
 import { PrismaService } from '../database/prisma/prisma.service';
+
+interface CreateCourseData {
+  title: string;
+}
 
 @Injectable()
 export class CoursesService {
@@ -12,6 +18,25 @@ export class CoursesService {
     return this.prisma.course.findUnique({
       where: {
         id,
+      },
+    });
+  }
+
+  async createCourse({ title }: CreateCourseData) {
+    const slug = slugify(title, { lower: true });
+
+    const courseWithSameSlug = await this.prisma.course.findUnique({
+      where: { slug },
+    });
+
+    if (courseWithSameSlug) {
+      throw new Error('Course already exists');
+    }
+
+    return this.prisma.course.create({
+      data: {
+        title,
+        slug,
       },
     });
   }
